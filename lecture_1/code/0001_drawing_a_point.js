c8878da1ae6957cd12ab81ec3bcd922b9c9cca6b
Sipondo <tt_robroek@hotmail.com>
2026-01-26 21:09:41 +0100

    lecture 1 media and code

---
+++ lecture_1/code/0001_drawing_a_point.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(400, 400);
+    background(220); // grey background
+    strokeWeight(10); // how thick the point is
+    point(200, 200);
+}
+
+function draw() {
+    // Static drawing
+}
---
+++ lecture_1/code/0003_drawing_round_shapes.js
@@ -0,0 +1,20 @@
+function setup() {
+    createCanvas(400, 400);
+    background(220); // grey background
+    
+    // Draw a circle
+    fill(255, 0, 0); // red colour
+    circle(100, 100, 80);
+    
+    // Draw an ellipse
+    fill(0, 255, 0); // green colour
+    ellipse(300, 100, 120, 80);
+    
+    // Draw another ellipse
+    fill(0, 0, 255); // blue colour
+    ellipse(200, 250, 80, 120);
+}
+
+function draw() {
+    // Static drawing
+}
